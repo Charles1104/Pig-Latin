2d4193fcc786a667eaf8465f68a81c048c146504
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-03-21 13:28:37 -1000

    All functions working

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+function firstVowel(string){
+  var firstVowelIndex = string.search(/[AEIOU]/i);
+  return firstVowelIndex;
+}
+
 function pigLatin(string) {
   var sentence = [];
   var words = [];
   var translated = [];
-  var indexFirstVowel = [];
 
-  // create an array where eahc element is a word of the sentence
+  // Check if the string is valid
+  if (string === "" || typeof string !== "string") {
+    throw new Error("Please enter a valid string.");
+  }
+
+  // create an array "sentence" where each element is a word of the sentence
   sentence = string.split(" ");
 
   for (var i = 0; i < sentence.length; i++ ){
@@ -12,44 +21,37 @@ function pigLatin(string) {
   // Goes over all the words into the sentence, put them in lowercase and split each letter in an array.
   words[i] = sentence[i].toLowerCase().split("");
 
-  if (words[i].indexOf("a") >= 0){
-    indexFirstVowel.push(words[i].indexOf("a"));
-  }
-  if (words[i].indexOf("e") >= 0){
-    indexFirstVowel.push(words[i].indexOf("e"));
-  }
-  if (words[i].indexOf("i") >= 0){
-    indexFirstVowel.push(words[i].indexOf("i"));
-  }
-  if (words[i].indexOf("u") >= 0){
-    indexFirstVowel.push(words[i].indexOf("u"));
-  }
-  if (words[i].indexOf("o") >= 0){
-    indexFirstVowel.push(words[i].indexOf("o"));
-  }
-
-  mini = Math.min.apply(null, indexFirstVowel);
-
-  console.log(words[i]);
-  console.log(indexFirstVowel);
-
-    if (words[i][0] === "a" || words[i][0] === "e" || words[i][0] === "i" || words[i][0] === "o" || words[i][0]=== "u") {
-      words[i] = words[i].join("") + "ay";
-      translated.push(words[i]);
-      indexFirstVowel = [];
+  // First case where the word starts with a vowel
+    if (words[i].join("").search(/[AEIOU]/i) === 0) {
+      words[i] = words[i].join("") + "-ay";
     }
+
+  // Second case where the word starts with a consonant
     else{
-      var start = words[i].slice(mini,words[i].length).join("");
-      var end = words[i].slice(0,mini).join("");
-      words[i] = start + end + "ay";
-      translated.push(words[i]);
-      indexFirstVowel = [];
+      var min = firstVowel(words[i].join(""));
+      var start = words[i].slice(min,words[i].length).join("");
+      var end = words[i].slice(0,min).join("");
+      words[i] = start + "-" + end + "ay";
+
     }
 
+  // Pushes the word modified in the translated array
+
+    translated.push(words[i]);
   }
 
-  return translated;
+  return translated.join(" ");
 }
 
-console.log(pigLatin("omo hjjaa llele"));
+
+
+function pigLatinReverse(piggedWord) {
+  if (piggedWord.search(/-?ay/g) <= 0) {
+      throw new Error("Not a valid Pig-Latin string");
+    }
+  return piggedWord.slice(piggedWord.search('-') + 1, -2 ) + piggedWord.slice( 0, piggedWord.search('-'));
+}
+
+console.log(pigLatinReverse("anana-bay"));
+
 
